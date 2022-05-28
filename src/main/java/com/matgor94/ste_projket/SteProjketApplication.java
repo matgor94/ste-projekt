c21979ba9942449798582dd28f340ee6f58214f6
matgor94 <maly316@vp.pl>
2022-05-28 18:47:25 +0200

    Stworzony szkielet w springboot

---
+++ src/main/java/com/matgor94/ste_projket/SteProjketApplication.java
@@ -0,0 +1,13 @@
+package com.matgor94.ste_projket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteProjketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteProjketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matgor94/ste_projket/SteProjketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matgor94.ste_projket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteProjketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
